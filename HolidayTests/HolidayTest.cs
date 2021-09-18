bd668b6f8ed4895b86f2fd0c914e8bcf970451fa
Jamie <jamie.wen.tw@gmail.com>
2021-09-18 11:59:49 +0800

    Complete prod code and  add test

--- HolidayTests/HolidayTest.cs
+++ HolidayTests/HolidayTest.cs
@@ -7,9 +7,32 @@ namespace HolidayTests
     public class HolidayTest
     {
         [Test]
-        public void Test1()
+        public void Today_Is_Xmas()
         {
-            Assert.True(true);
+            var hoilday = new HolidayForTest();
+            hoilday.Today = new DateTime(DateTime.Now.Year, 12, 25);
+            Assert.AreEqual("Merry Xmas", hoilday.IsXmas());
+
+        }
+
+        [Test]
+        public void Today_Is_Not_Xmas()
+        {
+            var hoilday = new Lib.HoildayService();
+            Assert.AreEqual("Today is not Xmas", hoilday.IsXmas());
+        }
+
+        private class HolidayForTest : Lib.HoildayService
+        {
+            private DateTime _today;
+            protected override DateTime GetToday()
+            {
+                return _today;
+            }
+
+            public DateTime Today { set => _today = value; }
+
+
         }
     }
 }
---
+++ Lib/HoildayService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    public class HoildayService
+    {
+        public string IsXmas()
+        {
+            DateTime xMas = GetToday();
+            if (xMas.Month == 12 && xMas.Day == 25)
+            {
+                return "Merry Xmas";
+            }
+            return "Today is not Xmas";
+        }
+        protected virtual DateTime GetToday()
+        {
+            return DateTime.Now;
+        }
+    }
+}
